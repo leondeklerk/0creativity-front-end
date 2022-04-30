d8a9e1c57cd80920b17734f72fb3da72106f2601
Safouane El Hilali <s.elhilali@student.tudelft.nl>
2022-05-01 00:38:38 +0200

    Vacations n shit

---
+++ components/SectionHeadingSimple.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="py-5">
+        <div class="pb-5 border-b border-gray-200">
+            <h2 class="leading-6 text-xl font-medium text-gray-900">
+                <slot>Sample Text</slot>
+            </h2>
+        </div>
+    </div>
+</template>
---
+++ pages/NewVacation.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-white h-screen"> 
+        <dark-nav-with-overlap title="Creating New Vacation">
+            <section-heading-simple>What's your maximum budget?</section-heading-simple>
+            <section-heading-simple>From when to when are you free?</section-heading-simple>
+            <section-heading-simple>Where do you want to go?</section-heading-simple>
+        </dark-nav-with-overlap>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
