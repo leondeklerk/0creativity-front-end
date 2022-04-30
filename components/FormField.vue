b7f123a21470aac5abc7ef09e11ec6e72d49ecfa
Leon de Klerk <developer@leondeklerk.com>
2022-04-30 19:14:58 +0200

    Fix forms
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- components/FormField.vue
+++ components/FormField.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 interface Props {
     label: string,
-    modelValue: string
+    modelValue: string,
+    type: string
 }
 
 const props = defineProps<Props>();
@@ -16,18 +17,18 @@ function onInput(event: Event) {
 </script>
 
 <template>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+  <div>
+    <div>
+      <label class="inline-block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
           {{props.label}}
       </label>
     </div>
-    <div class="md:w-2/3">
+    <div>
       <input
         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-        id="inline-full-name"
         @input="onInput"
         :value="props.modelValue"
+        :type="props.type"
       />
     </div>
   </div>
--- components/LoginView.vue
+++ components/LoginView.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="w-full max-w-sm">
-    <div class="md:flex md:items-center mb-6">
-      <form-field label="Username" v-model="username" />
-    </div>
-    <div class="md:flex md:items-center mb-6">
-      <form-field label="Password" v-model="password" />
-    </div>
-    <div class="md:flex md:items-center">
-      <form-button>Log in</form-button>
-    </div>
+  <div class="flex items-center justify-center">
+    <form class="w-full max-w-sm">
+      <div class="md:flex md:items-center mb-6">
+        <form-field type="text" label="Username" v-model="username" />
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <form-field type="password" label="password" v-model="password" />
+      </div>
+      <div class="md:block md:items-center">
+        <div class="md:w-1/3"></div>
+        <div class="md:w-2/3">
+          <form-button>
+            Sign Up
+          </form-button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
