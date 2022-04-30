55fb027638915d32aa77d0fffcd29175effa755d
Leon de Klerk <developer@leondeklerk.com>
2022-05-01 01:17:37 +0200

    Fix styling
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- pages/NewVacation.vue
+++ pages/NewVacation.vue
@@ -2,7 +2,8 @@
     <div class="bg-white h-screen">
         <dark-nav-with-overlap title="Creating New Vacation">
             <section-heading-simple>Budget</section-heading-simple>
-            <input-price /> <!-- Why doesn't this show up? -->
+            <input-price />
+            <!-- Why doesn't this show up? -->
             <section-heading-simple>When are you free?</section-heading-simple>
             <section-heading-simple>Where do you want to go?</section-heading-simple>
         </dark-nav-with-overlap>
